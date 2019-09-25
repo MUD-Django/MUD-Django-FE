3189632e062c3764e7c7b16f3acbc9e9917e76d4
ghungpham <pham.ghungj@gmail.com>
2019-09-25 16:25:57 -0400

    directions

--- mud-project/src/Components/Map.js
+++ mud-project/src/Components/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
 import { axiosWithAuth } from './axiosWithAuth'
+import Button from '@material-ui/core/Button';
 
 class Map extends Component {
 
@@ -9,7 +10,7 @@ class Map extends Component {
         title: '',
         description: '',
         players: [],
-        direction: '',
+        error_msg: '',
     }
 
     componentDidMount() {
@@ -28,8 +29,60 @@ class Map extends Component {
             console.log(`Login Error: ${err}`)
         })
     }
-    
 
+    goDirection = direction => {
+        axiosWithAuth()
+        .post('https://lambda-mud-test.herokuapp.com/api/adv/move/', direction)
+        .then(res => {
+            console.log(res)
+            this.setState({
+                name: res.data.name,
+                title: res.data.title,
+                description: res.data.description,
+                players: res.data.players,
+                error_msg: res.data.error_msg
+            })
+        })
+        .catch(err => {
+            console.log(`Login Error: ${err}`)
+        })
+    }
+    
+    goNorth = e => {
+        e.preventDefault()
+        const north = {
+            'direction': 'n'
+        }
+        this.goDirection(north)
+        
+    }
+
+    goSouth = e => {
+        e.preventDefault()
+        const south = {
+            'direction': 's'
+        }
+        this.goDirection(south)
+        
+    }
+
+    goWest = e => {
+        e.preventDefault()
+        const west = {
+            'direction': 'w'
+        }
+        this.goDirection(west)
+        
+    }
+
+    goEast = e => {
+        e.preventDefault()
+        const east = {
+            'direction': 'e'
+        }
+        this.goDirection(east)
+        
+    }
 
     render() {
         return (
@@ -38,6 +91,8 @@ class Map extends Component {
                 <h6>{this.state.name}</h6>
                 <h6>{this.state.title}</h6>
                 <h6>{this.state.description}</h6>
+
+                <Button onClick={this.goNorth}>N</Button>
 
             </div>
         );
