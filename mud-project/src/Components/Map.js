a49a96bdcdeb3defaaa857b72c2cdd77ec8f9cb9
ghungpham <pham.ghungj@gmail.com>
2019-09-25 02:50:33 -0400

    changed

--- mud-project/src/Components/Map.js
+++ mud-project/src/Components/Map.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom'
 
 class Map extends Component {
     render() {
@@ -10,4 +11,4 @@ class Map extends Component {
     }
 }
 
-export default Map;
+export default withRouter(Map);
