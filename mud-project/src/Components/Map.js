47bfc7f8c15414306d5c0f306b3fd142c715cc81
ghungpham <pham.ghungj@gmail.com>
2019-09-25 12:26:58 -0400

    axioswithAuth

--- mud-project/src/Components/Map.js
+++ mud-project/src/Components/Map.js
@@ -1,11 +1,43 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom'
+import { axiosWithAuth } from './axiosWithAuth'
 
 class Map extends Component {
+
+    state = {
+        name : '',
+        title: '',
+        description: '',
+        players: [] 
+    }
+
+    componentDidMount() {
+        axiosWithAuth()
+        .get('https://lambda-mud-test.herokuapp.com/api/adv/init/')
+        .then(res => {
+            console.log(res)
+            this.setState({
+                name: res.data.name,
+                title: res.data.title,
+                description: res.data.description,
+                players: res.data.players
+            })
+        })
+        .catch(err => {
+            console.log(`Login Error: ${err}`)
+        })
+    }
+    
+
+
     render() {
         return (
             <div>
                 Mapppppppp
+                <h6>{this.state.name}</h6>
+                <h6>{this.state.title}</h6>
+                <h6>{this.state.description}</h6>
+
             </div>
         );
     }
